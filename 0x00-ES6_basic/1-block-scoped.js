0f1aeccb80b53883d2aaf2c82a8c73389ede433e
VictorMaona <maonavictor@gmail.com>
2024-03-04 22:12:24 +0000

    updated

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,10 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  /* no-unused-vars eslint-disable */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
-  /* enable no-unused-vars for eslint */
 
   return [task, task2];
 }
