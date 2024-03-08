0243b2c7c7d8c686c9880c716eb4e64ae9620928
VictorMaona <maonavictor@gmail.com>
2024-03-08 02:48:49 +0000

    updated

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,13 @@
-/* the eslint-disable */
 import Car from './10-car';
 
 export default class EVCar extends Car {
-	constructor(brand, motor, color, range) {
-		super(brand, motor, color);
-		this._range = range;
-	}
+    constructor(brand, motor, color, range) {
+        super(brand, motor, color);
+        this._range = range;
+    }
 
-	cloneCar() {
-		return new Car();
-	}
+    cloneCar() {
+        const { _brand, _motor, _color } = this;
+        return new Car(_brand, _motor, _color);
+    }
 }
