aba50a25f453c745dda485576e028242b7b92747
VictorMaona <maonavictor@gmail.com>
2024-05-02 22:19:23 +0000

    updated

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,9 +1,9 @@
-const getPaymentTokenFromAPI = require("6-payment_token");
+const getPaymentTokenFromAPI = require("./6-payment_token");
 const {describe, it} = require("mocha");
 const expect = require("chai").expect;
 
 describe("getPaymentTokenFromAPI", function() {
-    it("Testing async with completed callback", function(done) {
+    it("Async testing with done callback", function(done) {
 	getPaymentTokenFromAPI(true)
 	    .then((data) => {
 		expect(data).to.have.property('data');
